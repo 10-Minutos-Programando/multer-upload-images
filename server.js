683c6ab08eda4d479bc4e902d4a0ede6895f906b
Mario Giron Martin <mariogironm@gmail.com>
2020-07-15 00:32:36 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
+const upload = multer({ dest: 'public/images/' })
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/upload', upload.single('imagen'), (req, res) => {
+    fs.renameSync(req.file.path, req.file.path + '.' + req.file.mimetype.split('/')[1]);
+    res.send('Se ha subido');
+});
+
+app.post('/uploadmult', upload.array('imagenes'), (req, res) => {
+    console.log(req.files);
+    res.send('Termina!');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
